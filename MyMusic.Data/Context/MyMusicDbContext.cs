f8c659a219620413224159bb967c493cacb61e90
Esraa Mohamed <esraaahmedaliahmed@gmail.com>
2020-07-10 17:09:26 +0200

    Adding EntityFramework Code First

---
+++ MyMusic.Data/Context/MyMusicDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusic.Core.Models;
+using MyMusic.Data.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMusic.Data.Context
+{
+    public class MyMusicDbContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public MyMusicDbContext(DbContextOptions<MyMusicDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MusicConfiguration());
+            modelBuilder.ApplyConfiguration(new ArtistConfiguration());
+
+        }
+
+    }
+}
---
+++ MyMusic.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using MyMusic.Core.Repositories;
+using MyMusic.Core.UnitOfWork;
+using MyMusic.Data.Context;
+using MyMusic.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Data.UnitOfWork
+{
+   public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MyMusicDbContext _context;
+        private IMusicRepository _musicRepository;
+        private IArtistRepository _artistRepository;
+
+        public UnitOfWork(MyMusicDbContext context)
+        {
+            this._context = context;
+        }
+        public IMusicRepository Musics => _musicRepository = _musicRepository ?? new MusicRepository(_context);
+
+        public IArtistRepository Artists => _artistRepository = _artistRepository ?? new ArtistRepository(_context);
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
