0589b065662a9a119243f7aff4cf07093724d589
Esraa Mohamed <esraaahmedaliahmed@gmail.com>
2020-07-10 15:36:30 +0200

    Add Repositories and UnitOfwork and

---
+++ MyMusic.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using MyMusic.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Core.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IMusicRepository Musics { get; }
+        IArtistRepository Artists { get; }
+        Task<int> CommitAsync();
+    }
+}
